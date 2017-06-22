c811df05a95a6f44d4a96b5cff142c5aff31979e
Joshua Richardson <joshuarichardson@Joshuas-MacBook-Pro.local>
2017-06-22 17:02:19 +0800

    According working

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ $(".menu-collapsed").click(function() {
   $(this).toggleClass("menu-expanded");
   $(".menu-text").toggleClass("menu-text-collapsed ");
 });
+
+$("a#buy-btn").click(function(e) {
+  e.preventDefault();
+  $(".cart").toggleClass("cart-display");
+});
