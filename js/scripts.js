0c4c64c8cee906b2955452658598e36e4d4380bb
Joshua Richardson <joshuarichardson@Joshuas-MacBook-Pro.local>
2017-06-24 14:10:56 +0800

    Header image for product page added

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,72 @@ $(".menu-collapsed").click(function() {
 
 $("a#buy-btn").click(function(e) {
   e.preventDefault();
-  $(".cart").toggleClass("cart-display");
+  $(".buy-form").toggleClass("buy-display");
+});
+
+// $("a#buy-btn").click(function(e) {
+//   e.preventDefault();
+//   $(".cart").toggleClass("cart-display");
+// });
+
+
+$(".display-prod-full").click(function(e) {
+  e.preventDefault();
+  $(".product-list-squares").hide();
+  $(".product-list-full").show();
+});
+
+$(".display-prod-split").click(function(e) {
+  e.preventDefault();
+  $(".product-list-squares").show();
+  $(".product-list-full").hide();
+});
+
+
+// adjustHeight is the function that makes the objects squares
+// (by setting each one's height equal to its width)
+function adjustHeight() {
+	var myWidth = jQuery('.square-product').width();
+	var myString = myWidth + 'px';
+	jQuery('.square-product').css('height', myString);
+	return myHeight;
+}
+
+
+
+
+
+  $(document).ready(function(){
+
+var quantitiy=0;
+   $('.quantity-right-plus').click(function(e){
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+            $('#quantity').val(quantity + 1);
+
+
+            // Increment
+
+    });
+
+     $('.quantity-left-minus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+            // Increment
+            if(quantity>0){
+            $('#quantity').val(quantity - 1);
+            }
+    });
+
 });
